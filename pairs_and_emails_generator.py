bf2617d6982d7a6484e3704c9f1462d9e5fe7434
Baptiste Lafoux <38565243+BaptisteLafoux@users.noreply.github.com>
2022-02-09 11:28:20 +0100

    update

--- pairs_and_emails_generator.py
+++ pairs_and_emails_generator.py
@@ -6,11 +6,12 @@ Created on Tue Feb  8 14:49:07 2022
 @author: baptistelafoux
 """
 import pandas as pd 
-from itertools import product, permutations
-import random 
+
 from sklearn.utils import shuffle
 from urllib.parse import quote
 import webbrowser
+
+gen_email = False
 
 sheet_url = 'https://docs.google.com/spreadsheets/d/1i5hRWAO--EMEOloPSciI-rpKYIwD85cX-op7cvR2U8c/edit#gid=324996868'
 url = sheet_url.replace('/edit#gid=', '/export?format=csv&gid=')
@@ -20,17 +21,16 @@ df = pd.read_csv(url)
 nfs = df[df[df.columns[-1]] == 'Non-French-speaker']
 fs  = df[df[df.columns[-1]] == 'French-speaker']
 
+def mailto(recipients, subject, body):
+    webbrowser.open("mailto:%s?subject=%s&body=%s" %
+        (recipients, quote(subject), quote(body)))
 
-def gen(email,):
-    mailto(email, "[tandem] First tandem", body_template) 
+def gen(email, body):
+    mailto(email, "[tandem] First tandem", body) 
     
-for i, pair in enumerate(zip(shuffle(nfs).T, shuffle(fs).T)):
-    #print(i, pair)
-    
-    df_temp = df.loc[list(pair)]
-    
-    
-    body_template = f"""Hello {' & '.join(list(df_temp['First name']))} !
+def body(names):
+    return f"""
+Hello {' & '.join(names)} !
 
 I am happy to announce that you will be together for the first sessions of tandem. 
 As we discussed at the coffee meeting last week, you can meet once a week for 20-30min, to discuss a topic of your choice, in the language of your choice. 
@@ -46,13 +46,33 @@ Please feel free to get in touch with us if you have any trouble or question in 
 
 Best, 
 Andrea & Baptiste"""
-
-
-    gen(','.join(list(df_temp['Mail address '])))
+    
+def main():   
+    
+    pairs = list(zip(shuffle(nfs).T, shuffle(fs).T)) 
+    
+    for pair in pairs : print((df.loc[list(pair)]['First name'] + ' ' + df.loc[list(pair)]['Last Name']).values)
+    
+    answer = input('Want to keep these pairs and generate the emails ? [y/n]\t')
+    if answer == 'y' : 
+        pass
+    elif answer == 'n':
+        return None
+    
+    for i, pair in enumerate(pairs):
         
+        df_temp = df.loc[list(pair)]
+        
+        names = list(df_temp['First name'])
+        body_text = body(names)
+        
+        recipients = ','.join(list(df_temp['Mail address ']))
+    
+        if gen_email : gen(recipients, body_text)
+            
+        
+if __name__ == '__main__':
+    main()
 
-def mailto(recipients, subject, body):
-    webbrowser.open("mailto:%s?subject=%s&body=%s" %
-        (recipients, quote(subject), quote(body)))
 
 
